76c1d0e2c91538c4750ed3cb1865cf65ee8fbb57
Takeru Sato <type.in.type@gmail.com>
2020-03-23 08:50:37 +0900

    build: update to scala 2.12.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val Ver = new {
     val `scala2.13` = "2.13.1"
-    val `scala2.12` = "2.12.10"
+    val `scala2.12` = "2.12.11"
 
     val kindProjector = "0.11.0"
 
